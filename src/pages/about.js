e214b57d62ae8b6b9ea443e5eb8dd06116321dc3
abhinav2188 <abhinav.mca19.du@gmail.com>
2020-05-25 01:15:26 +0530

    automatic page routing, gatsby-config, css modules

---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Container from "../components/container";
+
+export default function About(props) {
+    return(
+        <Container>
+            <h1>About React Gatsby</h1>
+            <p>A static site generator powered by react</p>
+        </Container>
+    )
+}
---
+++ src/pages/contact.js
@@ -0,0 +1,8 @@
+import React from "react"
+import { Link } from "gatsby"
+
+export default function Contact() {
+  return <div>
+  <Link to="/">Home</Link>
+    Contact page    </div>
+}
