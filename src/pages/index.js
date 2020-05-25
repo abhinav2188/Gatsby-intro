6c8c2144469faaa32e10ce252cf5422ac492da38
abhinav2188 <abhinav.mca19.du@gmail.com>
2020-05-25 12:41:44 +0530

    added transformer plugin: fetching data from markdown files

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,42 @@
 import React from "react"
-import { Link , graphql} from "gatsby"
+import { graphql} from "gatsby"
 import Container from "../components/container"
 
 export default function Home({data}) {
   return (
     <Container>
-      <Link to="/contact/">Contact</Link>
-      Hello world!
       <p>
       {data.site.siteMetadata.title}
       </p>
+      <p>posts {data.allMarkdownRemark.totalCount}</p>
+      {data.allMarkdownRemark.edges.map(({node},index) => (
+        <div>
+          <h3>{node.frontmatter.title} - {node.frontmatter.date}</h3>
+          <p>{node.excerpt}</p>
+        </div>
+      ) )}
     </Container>
   )
 }
 
 export const query = graphql `
   query{
-    site{
-      siteMetadata{
+    allMarkdownRemark(sort: {fields: frontmatter___date, order: DESC}) {
+      edges {
+        node {
+          id
+          excerpt(truncate: true, pruneLength: 50, format: PLAIN)
+          html
+          frontmatter {
+            title
+            date
+          }
+        }
+      }
+      totalCount
+    }
+    site {
+      siteMetadata {
         title
       }
     }
